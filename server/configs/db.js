000f434525738d926cbef65bb4ff544be9cdfc2f
STCzao <Santicoronel0183@gmail.com>
2025-06-21 19:48:41 -0300

    Empezamos la autenticacion de clerk para el Backend

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose.connection.on('connected', () => console.log('Database Connected'));
-    await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
+    await mongoose.connect(process.env.MONGODB_URI);
   } catch (error) {
     console.log(error.message);
   }
