0d8683f206e1561a4f2bf0764a61ec6c1f43e91f
STCzao <Santicoronel0183@gmail.com>
2025-06-23 15:52:49 -0300

    Solucionando errores del deploy

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,56 +1,51 @@
-import js from "@eslint/js";
-import User from "../models/User.js";
 import { Webhook } from "svix";
+import User from "../models/User.js";
+import connectDB from "../configs/db.js";
 
 const clerkWebhooks = async (req, res) => {
   try {
-    //Create a Svix instance with clerk webhook secret.
-    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+    await connectDB();
 
-    //Getting Headers
+    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
     const headers = {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     };
 
-    //Veryfing Headers
-    await whook.verify(JSON.stringify(req.body), headers);
+    const payload = req.body.toString(); // 👈 importante
+    const evt = wh.verify(payload, headers);
 
-    //Getting Data from request body
-    const { data, type } = req.body;
+    const { data, type } = evt;
 
     const userData = {
       _id: data.id,
-      email: data.email_addresses[0].email_addresses,
-      username: data.first_name + " " + data.last_name,
+      email: data.email_addresses[0].email_address,
+      username: `${data.first_name} ${data.last_name}`,
       image: data.image_url,
     };
 
-    //Switch Cases for different Events
     switch (type) {
-      case "user.created": {
+      case "user.created":
         await User.create(userData);
         break;
-      }
-
-      case "user.updated": {
-        await User.findByIDAndUpdate(data.id, userData);
+      case "user.updated":
+        await User.findByIdAndUpdate(data.id, userData);
         break;
-      }
-      case "user.deleted": {
-        await User.findByIDAndDelete(data.id);
+      case "user.deleted":
+        await User.findByIdAndDelete(data.id);
         break;
-      }
-
       default:
         break;
     }
-    res.json({ success: true, message: "Webhook Recieved" });
-  } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message: error.message });
+
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.error("Webhook error:", err.message);
+    res.status(500).json({ success: false, message: err.message });
   }
 };
 
 export default clerkWebhooks;
+
