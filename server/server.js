b6fe0c92b34b0d5d254c5d457875eb07af543677
STCzao <Santicoronel0183@gmail.com>
2025-06-25 08:00:50 -0300

    Probando la conexion a MongoDB

--- server/server.js
+++ server/server.js
@@ -4,21 +4,20 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRoutes from "./routes/users.js"; // <--- importa las rutas
 
 connectDB();
 
 const app = express();
-app.use(cors()); // Enable Cross-Origin Resource Sharing
-
-//Middleware
+app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 
-// API to listen to Clerl Webhooks
-app.use("/api/clerk", clerkWebhooks)
+// Rutas
+app.use("/api/clerk", clerkWebhooks);
+app.use("/api/users", userRoutes); // <--- monta las rutas de usuarios
 
 app.get("/", (req, res) => res.send("API is working"));
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
